99fbd8023406786532dea8472a0b93e8f83dd34e
patrickstevens24 <patrick.stevens@comcast.net>
2017-02-07 09:45:35 -0800

    Initial commit

---
+++ app/src/androidTest/java/pws24/uw/tacoma/edu/recycledpc/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package pws24.uw.tacoma.edu.recycledpc;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("pws24.uw.tacoma.edu.recycledpc", appContext.getPackageName());
+    }
+}
