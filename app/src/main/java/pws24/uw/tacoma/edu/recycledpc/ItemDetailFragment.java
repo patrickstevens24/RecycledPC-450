a8c137d324d1c1c11ac0f65718e654af515a9c63
ExGenere <panlilioarthur@yahoo.com>
2017-03-05 19:40:10 -0800

    connected sellers to item added email

--- app/src/main/java/pws24/uw/tacoma/edu/recycledpc/ItemDetailFragment.java
+++ app/src/main/java/pws24/uw/tacoma/edu/recycledpc/ItemDetailFragment.java
@@ -6,6 +6,7 @@
 
 package pws24.uw.tacoma.edu.recycledpc;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
@@ -15,11 +16,20 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 
 import pws24.uw.tacoma.edu.recycledpc.item.ItemContent;
 
@@ -47,12 +57,16 @@ public class ItemDetailFragment extends Fragment {
     private TextView mCourseIdTextView;
     private TextView mCourseShortDescTextView;
     private TextView mCourseLongDescTextView;
+    private TextView mCourseSellerIDTextView;
 
     private ImageView mItemImageView;
 
+    private String mContact;
 
     private OnListFragmentInteractionListener mListener;
     public final static String COURSE_ITEM_SELECTED = "course_selected";
+
+    private static final String SELLER_URL = "http://cssgate.insttech.washington.edu/~_450bteam10/getSeller.php?";
 
     /**
      * empty constructor
@@ -80,9 +94,24 @@ public class ItemDetailFragment extends Fragment {
         mCourseIdTextView = (TextView) view.findViewById(R.id.course_item_id);
         mCourseShortDescTextView = (TextView) view.findViewById(R.id.course_short_desc);
         mCourseLongDescTextView = (TextView) view.findViewById(R.id.course_long_desc);
+        mCourseSellerIDTextView = (TextView) view.findViewById(R.id.sellerID);
         mItemImageView = (ImageView) view.findViewById(R.id.imageView3);
-
-
+        Button button = (Button) view.findViewById(R.id.contact_owner);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("message/rfc822");
+                i.putExtra(Intent.EXTRA_EMAIL  , new String[]{mContact});
+                i.putExtra(Intent.EXTRA_SUBJECT, "subject of email");
+                i.putExtra(Intent.EXTRA_TEXT   , "body of email");
+                try {
+                    startActivity(Intent.createChooser(i, "Send mail..."));
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(getContext(), "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         return view;
     }
@@ -96,8 +125,19 @@ public class ItemDetailFragment extends Fragment {
         if (item != null) {
             mCourseIdTextView.setText(item.getId());
             mCourseShortDescTextView.setText(item.getShortDesc());
-            mCourseLongDescTextView.setText(item.getLongDesc() + item.getSellerID());
+            mCourseLongDescTextView.setText(item.getLongDesc());
             String imagePath = item.getPath();
+            StringBuilder sb = new StringBuilder(SELLER_URL);
+            sb.append("sellerID=");
+            String sellerID = item.getSellerID() + "";
+            try {
+                sb.append(URLEncoder.encode(sellerID, "UTF-8"));
+            }catch(Exception e) {
+
+            }
+            String url = sb.toString();
+            sb.append(item.getSellerID());
+            new ItemDetailFragment.SellerInfo().execute(new String[]{url});
             new DownloadImageTask(mItemImageView).execute(imagePath);
         }
     }
@@ -171,5 +211,58 @@ public class ItemDetailFragment extends Fragment {
             bmImage.setImageBitmap(result);
         }
     }
+
+    private class SellerInfo extends AsyncTask<String, Void, String> {
+
+        protected String doInBackground(String... urls) {
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            for (String url:urls){
+                try{
+                    URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+
+                    InputStream conent = urlConnection.getInputStream();
+
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(conent));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        response += s;
+                    }
+                } catch (Exception e) {
+                    response = "Unable to add course, Reason: "
+                            + e.getMessage();
+                }finally {
+                    if(urlConnection != null)
+                        urlConnection.disconnect();
+                }
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            // Something wrong with the network or the URL.
+            if (result.startsWith("Unable to")) {
+                Toast.makeText(getContext(), result, Toast.LENGTH_LONG)
+                        .show();
+                return;
+            }
+            try {
+                JSONObject obj = new JSONObject(result);
+                mCourseSellerIDTextView.setText(obj.getString("email") + "\n" + obj.getString("firstName") + " " + obj.getString("lastName"));
+                mContact = obj.getString("email");
+
+
+            } catch (JSONException e){
+            }
+
+        }
+
+
+    }
+
+
 
 }
