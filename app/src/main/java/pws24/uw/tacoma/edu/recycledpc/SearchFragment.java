eb593f7108334c043065e8412947ca3ecd3ee3cd
patrickstevens24 <patrick.stevens@comcast.net>
2017-03-05 17:25:21 -0800

    Added Search

---
+++ app/src/main/java/pws24/uw/tacoma/edu/recycledpc/SearchFragment.java
@@ -0,0 +1,260 @@
+/*
+ * Recycled PC
+ * Team 10
+ * TCSS 405b- Spring 2017
+ */
+
+package pws24.uw.tacoma.edu.recycledpc;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import pws24.uw.tacoma.edu.recycledpc.item.ItemContent;
+
+/**
+ * A fragment representing a list of Items.
+ * <p/>
+ * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
+ * interface.
+ *
+ * @author Patrick Stevens
+ */
+public class SearchFragment extends Fragment {
+
+
+    private static final String ARG_COLUMN_COUNT = "column-count";
+
+    private int mColumnCount = 2;
+
+    private EditText mSearchEditText;
+    private OnListFragmentInteractionListener mListener;
+    private String url;
+    private RecyclerView mRecyclerView;
+
+    public SearchFragment() {
+
+    }
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public SearchFragment(String pls) {
+        url = pls;
+    }
+
+
+    @SuppressWarnings("unused")
+    public static ItemFragment newInstance(int columnCount) {
+        ItemFragment fragment = new ItemFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_COLUMN_COUNT, columnCount);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
+        }
+    }
+    /**
+     * Builds the url to connect to the php to add an item
+     *
+     * @param v is the view
+     * @return the url to connec to the php file
+     */
+    private String buildCourseURL(View v) {
+
+        StringBuilder sb = new StringBuilder(url);
+
+        try {
+
+            String courseId = mSearchEditText.getText().toString();
+            sb.append("&title=");
+            sb.append(courseId);
+
+
+
+        }
+        catch(Exception e) {
+            Toast.makeText(v.getContext(), "Something wrong with the url" + e.getMessage(), Toast.LENGTH_LONG)
+                    .show();
+        }
+        return sb.toString();
+    }
+
+    public static void message(String s) {
+
+    }
+
+
+    /**
+     * Downloads the item information from the database so that it can be displayed in a list.
+     *
+     */
+    private class DownloadCoursesTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            for (String url : urls) {
+                try {
+                    URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+
+                    InputStream content = urlConnection.getInputStream();
+
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        response += s;
+                    }
+
+                } catch (Exception e) {
+                    response = "Unable to download the list of courses, Reason: "
+                            + e.getMessage();
+                } finally {
+                    if (urlConnection != null)
+                        urlConnection.disconnect();
+                }
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            // Something wrong with the network or the URL.
+            if (result.startsWith("Unable to")) {
+                Toast.makeText(getActivity().getApplicationContext(), "TEST", Toast.LENGTH_LONG)
+                        .show();
+                return;
+            }
+
+            List<ItemContent> courseList = new ArrayList<ItemContent>();
+            result = ItemContent.parseCourseJSON(result, courseList);
+            // Something wrong with the JSON returned.
+            if (result != null) {
+                Toast.makeText(getActivity().getApplicationContext(), "Item Not Found | Try Searching Again", Toast.LENGTH_LONG)
+                        .show();
+                return;
+            }
+
+            // Everything is good, show the list of courses.
+            if (!courseList.isEmpty()) {
+                mRecyclerView.setAdapter(new MySearchRecyclerViewAdapter(courseList, mListener));
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_search_list, container, false);
+
+        // Set the adapter
+        if (view instanceof RecyclerView) {
+            Context context = view.getContext();
+            mRecyclerView = (RecyclerView) view;
+            if (mColumnCount <= 1) {
+                mRecyclerView.setLayoutManager(new LinearLayoutManager(context));
+            } else {
+                mRecyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+            }
+            //download(COURSE_URL);
+            DownloadCoursesTask task = new DownloadCoursesTask();
+            task.execute(new String[]{url});
+        }
+        /**
+        Button addCourseButton = (Button) view.findViewById(R.id.search_button);
+        addCourseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = buildCourseURL(view);
+                download(url);
+
+            }
+        });
+         */
+
+        FloatingActionButton floatingActionButton = (FloatingActionButton)
+                getActivity().findViewById(R.id.fab);
+        floatingActionButton.show();
+
+
+        return view;
+    }
+
+
+    private void download(String courseUrl) {
+        DownloadCoursesTask task = new DownloadCoursesTask();
+        task.execute(new String[]{courseUrl});
+    }
+
+
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnListFragmentInteractionListener) {
+            mListener = (OnListFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnListFragmentInteractionListener");
+        }
+    }
+
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnListFragmentInteractionListener extends ItemFragment.OnListFragmentInteractionListener {
+        void onListFragmentInteraction(ItemContent item);
+    }
+
+
+}
